78d5e29d6e337bd8145aeb092d80fc1d870f1502
Muhammad Usman <usmanvirgo4@gmail.com>
2023-11-29 04:49:23 +0500

    Initial Changes

--- src/main/java/com/example/actuatordemo/ActuatorDemoApplication.java
+++ src/main/java/com/example/actuatordemo/ActuatorDemoApplication.java
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActuatorDemoApplication {
 
+
+
+
+
+
+
+
+
+
+	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorDemoApplication.class, args);
 	}
